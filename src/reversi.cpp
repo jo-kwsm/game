05889f9642243bf6598df585f6b2e40009a20a88
jo-kwsm <ytk.kwsm@gmail.com>
2020-07-09 22:34:05 +0900

    裏返す処理とコンピュータの手を探す処理を実装

--- src/reversi.cpp
+++ src/reversi.cpp
@@ -7,6 +7,8 @@ class reversi {
 private:
   int stage[8][8];
   char change[3]={' ','o','x'};
+  int idy[8]={1,1,1,0,-1,-1,-1,0};
+  int idx[8]={-1,0,1,1,1,0,-1,-1};
   int turn_player=1;
   vector<pair<int,int>> candidate;
 public:
@@ -39,12 +41,11 @@ public:
   }
 
   void display() {
-    cout << " a|b|c|d|e|f|g|h" << endl;
+    cout << " |a|b|c|d|e|f|g|h|" << endl;
     for(int i=0;i<8;i++) {
-      cout << i;
+      cout << i+1 << "|";
       for(int j=0;j<8;j++) {
-        if(j) cout << "|";
-        cout << change[stage[i][j]];
+        cout << change[stage[i][j]] << "|";
       }
       cout << endl;
     }
@@ -59,21 +60,49 @@ public:
     return false;
   }
 
-  void search() {
+  void search(int v) {
     candidate.clear();
     for(int i=0;i<8;i++) {
       for(int j=0;j<8;j++) {
-
+        if(stage[i][j]) continue;
+        if(process(i,j,v,0)) {
+          candidate.push_back({i,j});
+        }
       }
     }
   }
 
-  void process(int y, int x) {
-
+  bool process(int y, int x, int v, int reverse=1) {
+    bool res=false;
+    for(int i=0;i<8;i++) {
+      int ny=y+idy[i];
+      int nx=x+idx[i];
+      if(ny>=8&&nx>=8&&nx<0&&ny<0) continue;
+      if(stage[ny][nx]==v) continue;
+      int f=0;
+      while(ny<8&&nx<8&&nx>=0&&ny>=0&&stage[ny][nx]!=0) {
+        ny+=idy[i];
+        nx+=idx[i];
+        if(stage[ny][nx]==v) {
+          f=1;
+          break;
+        }
+      }
+      if(f) {
+        while(ny!=y||nx!=x) {
+          if(reverse) stage[ny][nx]=v;
+          ny-=idy[i];
+          nx-=idx[i];
+        }
+        if(reverse) stage[ny][nx]=v;
+        res=true;
+      }
+    }
+    return res;
   }
 
   void com() {
-    search();
+    search(2);
     if(candidate.size()==0) {
       cout << "com can't play" << endl;
       return;
@@ -81,7 +110,7 @@ public:
     int idx=rand()%candidate.size();
     int y=candidate[idx].first;
     int x=candidate[idx].second;
-    process(y,x);
+    process(y,x,2,1);
   }
 
   pair<int,int> input() {
@@ -106,9 +135,9 @@ public:
   void turn() {
     display();
     if(turn_player==1) {
-      search();
+      search(1);
       pair<int,int> hand=input();
-
+      process(hand.first,hand.second,1);
     }
     else {
       cout << "com turn" << endl;
